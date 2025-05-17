974b2e9537ac0037ed74c05ce61109ad580011b3
Pallabi Kundu <pallabi2307@gmail.com>
2025-05-17 21:13:27 +0530

    adding annotation Restcontroller

--- src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
@@ -3,8 +3,10 @@ package com.javatechie.github_cicd_actions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
