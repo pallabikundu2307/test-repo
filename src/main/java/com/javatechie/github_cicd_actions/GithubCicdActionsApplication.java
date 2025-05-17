002803e554495163af2f1777a998e4e1b7696797
Pallabi Kundu <pallabi2307@gmail.com>
2025-05-17 21:19:27 +0530

    changing message in controller

--- src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "Welcome to cicd pipeline demo!";
 	}
 
 	public static void main(String[] args) {
